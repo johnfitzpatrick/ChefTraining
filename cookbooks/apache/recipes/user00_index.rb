38a04dabf504caa5bf365070187a85d70b23d401
Manny Toledo <mtoledo@adobe.com>
2014-04-15 11:23:44 -0700

    Revert "Changed userid to userid38", maybe this worked
    This reverts commit bdc3f4e8fa5f972f8dd6194ee71b1693511fea93.
    
    Conflicts:
    	cookbooks/apache/files/default/user00_index.html
    	cookbooks/apache/recipes/user00_index.rb

--- cookbooks/apache/recipes/user00_index.rb
+++ cookbooks/apache/recipes/user00_index.rb
@@ -1,4 +1,4 @@
 cookbook_file "/var/www/html/user38_index.html" do
   source "user00_index.html"
   mode 00644
-end
+eni
--- cookbooks/apache/recipes/user38_index.rb
+++
@@ -1,4 +0,0 @@
-cookbook_file "/var/www/html/user38_index.html" do
-  source "user38_index.html"
-  mode 00644
-end
