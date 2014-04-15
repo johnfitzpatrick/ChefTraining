2743182edb3a3826dcba73eb019aabe57a2b7dba
James Setaro <james.setaro@pearson.com>
2014-04-15 12:55:35 -0500

    update recipe.

--- cookbooks/apache/recipes/user00_index.rb
+++ cookbooks/apache/recipes/user00_index.rb
@@ -1,4 +1,4 @@
-cookbook_file "/var/www/html/user00_index.html" do
-  source "user00_index.html"
+cookbook_file "/var/www/html/user03_index.html" do
+  source "user03_index.html"
   mode "0644"
 end
