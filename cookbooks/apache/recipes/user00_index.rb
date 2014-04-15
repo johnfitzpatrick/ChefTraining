bdc3f4e8fa5f972f8dd6194ee71b1693511fea93
Manny Toledo <mtoledo@adobe.com>
2014-04-15 10:51:11 -0700

    Changed userid to userid38

--- cookbooks/apache/recipes/user00_index.rb
+++
@@ -1,4 +0,0 @@
-cookbook_file "/var/www/html/user00_index.html" do
-  source "user00_index.html"
-  mode "0644"
-end
---
+++ cookbooks/apache/recipes/user38_index.rb
@@ -0,0 +1,4 @@
+cookbook_file "/var/www/html/user38_index.html" do
+  source "user38_index.html"
+  mode 00644
+end
