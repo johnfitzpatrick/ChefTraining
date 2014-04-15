a5a42e6ffb08729656800fba74e34ee6431f925f
Manny Toledo <mtoledo@adobe.com>
2014-04-15 10:54:41 -0700

    Update metadata

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,6 +1,6 @@
 name             'apache'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Manny Toledo'
+maintainer_email 'corp@email.com'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
